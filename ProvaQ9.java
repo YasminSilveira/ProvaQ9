ca51b36f91ce985ce4dfaa2f3b863ac3bc53c914
Yasmin <64554444+YasminSilveira@users.noreply.github.com>
2020-07-10 18:37:33 -0300

    ;D

---
+++ ProvaQ9.java
@@ -0,0 +1,35 @@
+
+package provaq9;
+
+/**
+ *
+ * @author yasmi
+ */
+public class ProvaQ9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+         int ValoresPositivos, cont , x;
+
+        cont = 1;
+        ValoresPositivos = 0;
+
+
+        while(cont <= 10){
+            cont++;
+            x = Integer.parseInt(JOptionPane.showInputDialog("Insira qualquer valor, por gentileza: "));
+
+            if(x >=0){
+                ValoresPositivos++;
+            }
+
+      }
+
+        JOptionPane.showMessageDialog(null , "A totalidade de valores positivos inseridos foi de: "+ValoresPositivos);
+    }
+
+}
